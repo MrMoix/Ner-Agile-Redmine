a6ebf28ba6a64ce124b3c06d41709151a2d20001
Baptiste Ostertag <45662303+baptisteostertag@users.noreply.github.com>
2022-01-06 14:08:58 +0100

    Delete SeparateForms directory

--- SeparateForms/NER-Agile/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NER_Agile.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
